223f018f70de8fa3b41bdf0ce0c100ae6ae2228e
Smart-Computer <Smart-Computer@DESKTOP-ECM31QT>
2024-11-12 15:08:25 +0530

    hello commot

--- gitProject02/src/main/java/com/nilesh/gitProject02/App.java
+++ gitProject02/src/main/java/com/nilesh/gitProject02/App.java
@@ -6,5 +6,6 @@ package com.nilesh.gitProject02;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello");
     }
 }
